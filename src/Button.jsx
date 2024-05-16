4a4e32c5483748d2adf32df993494d3b98ac0af7
Mohammed Faizan <mohdfaizan.zprb2@gmail.com>
2024-05-16 16:20:51 +0530

    just before changing

---
+++ src/Button.jsx
@@ -0,0 +1,8 @@
+export const CustomBtn = () => {
+  return <div className="bg-red-300">Custom button</div>;
+};
+const CustomBtn2 = () => {
+  return <div className="bg-red-300">Custom button 2</div>;
+};
+
+export {}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export {} from "./Button.jsx"
+
+export const helloThem = () =>{
+  console.log("hi")
+}
+
+// export const CustomBtn = () => {
+//   return <div className="bg-red-300">Custom button</div>;
+// };
