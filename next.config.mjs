bb4cd57287173d6da32a76b0428ccf636bd299de
Mohammed Faizan <mohdfaizan.zprb2@gmail.com>
2024-07-04 12:00:46 +0530

    mdx working fine

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
-// import MDX from '@next/mdx'
-// const withMDX = MDX()
+import MDX from "@next/mdx";
+const withMDX = MDX();
+// const withMDX = require("@next/mdx")();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // images: {
   //   domains: ["lh3.googleusercontent.com"],
   // },
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
+// module.exports = withMDX(nextConfig)
--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+
 const page = () => {
   return (
-    <div>page</div>
+    <div>page
+      
+    </div>
+
   )
 }
 
