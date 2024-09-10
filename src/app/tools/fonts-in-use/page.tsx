9924dfa52f2fb8e1944f919b7bdcb0020a71116d
Likitha N <likithanagaraj2004@gmail.com>
2024-09-10 11:42:48 +0530

    resolved the contentlayer issue

--- src/app/tools/fonts-in-use/page.tsx
+++ src/app/tools/fonts-in-use/page.tsx
@@ -10,7 +10,7 @@ const page = () => {
       <section className="flex gap-3 flex-wrap">
         {fontsCombinations.map((fontCombination, i) => (
           <div key={i}
-            className="flex flex-col  rounded-xl border bg-card text-card-foreground shadow transition-colors  sm:p-5 w-[299px]"
+            className="flex flex-col  rounded-xl border bg-card text-card-foreground shadow transition-colors  p-5 w-[299px]"
           >
             <textarea className={`text-2xl h-12 w-auto  font-bold text-card-foreground resize-none  ${fontCombination.font1.font.className}`}>{fontCombination.font1.name}</textarea>
             <textarea className={`opacity-90 h-12 text-smtext-card-foreground resize-none  ${fontCombination.font1.font.className} ${fontCombination.font2.font.className} `}>{fontCombination.font2.name}</textarea>
