604bc8b3ffc5a683621854fbb5cdd5744cd2064c
muhammedtahir1 <mohammedtahir65080@gmail.com>
2024-09-11 20:11:53 +0530

    added 2 components from template salient - hero3, testimonial3

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,8 +1,13 @@
+import HeroSection3 from '@/registry/components/HeroSections/hero-section3'
+import { Testimonials3 } from '@/registry/components/testimonials/Testimonials3'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      {/* <HeroSection3/> */}
+      {/* <Testimonials3/> */}
+    </div>
   )
 }
 
