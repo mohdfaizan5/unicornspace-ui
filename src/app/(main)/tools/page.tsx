77babab54aebc39e755e5926eb7f8be6e517e8c3
Likitha N <likithanagaraj2004@gmail.com>
2024-09-20 17:06:03 +0530

    made some changes in tool wrapper and add few images ss

--- src/app/(main)/tools/page.tsx
+++ src/app/(main)/tools/page.tsx
@@ -28,8 +28,7 @@ const tools = [
     name: "Fonts in Use",
     description: "Check out great some font combinations in use",
     url: "/tools/fonts-in-use",
-    image:
-      "https://www.magicpattern.design/_next/image?url=https%3A%2F%2Fstorage.googleapis.com%2Fbrandflow-bucket%2Fmagipattern%2Fmockups%2Fmesh-gradients.jpg&w=1080&q=75",
+    image: "/assets/tools/font-in-use.png",
     isPublished: true,
     beta: false,
   },
@@ -38,8 +37,7 @@ const tools = [
     name: "Readme generator",
     description: "Generate a readme file for your project",
     url: "/tools/readme-generator",
-    image:
-      "https://www.magicpattern.design/_next/image?url=https%3A%2F%2Fstorage.googleapis.com%2Fbrandflow-bucket%2Fmagipattern%2Fmockups%2Fmesh-gradients.jpg&w=1080&q=75",
+    image: "/assets/tools/colors-palettes.png",
     isPublished: true,
     beta: true,
   },
@@ -93,40 +91,31 @@ const page = () => {
           tools.map(
             (tool, i) =>
               tool.isPublished && (
-                <section className="w-96">
+                <Link href={`${tool.url}`} className="w-96">
                   <div
                     key={i}
-                    className="flex flex-col items-start gap-2 rounded-xl border bg-card p-2 text-card-foreground shadow transition-colors hover:bg-muted/50 md:py-5 relative"
+                    className="flex flex-col items-start gap-2 rounded-xl border bg-card p-2 text-card-foreground shadow transition-colors hover:bg-muted/50 md:py-5 relative h-96"
                   >
-                    <div className="space-x-3">
+                    <div className="h-[80%] mx-auto">
                       <Image
-                        className="w-full"
+                        className="w-full h-[80%] object-fill mx-auto"
                         src={tool.image}
                         alt={tool.name}
                         width={300}
                         height={300}
                       />
-
-                      {/* <Link href={tool.url}>
-                      <Button size={"sm"}>
-                        Browser Tool <ArrowRight size={16} />
-                      </Button>
-                    </Link>
-                    <Button size={"sm"} variant={"secondary"}>
-                      Documentation
-                    </Button> */}
                     </div>
 
-                    <section key={i} className="mt-3 mx-3">
+                    <div key={i} className="mt-3 mx-3">
                       <h1 className="text-2xl font-bold font-passion ">
                         {tool.name}
                       </h1>
                       <p className="max-w-80 text-[13px] tracking-tight ">
                         {tool.description}
                       </p>
-                    </section>
+                    </div>
                   </div>
-                </section>
+                </Link>
               )
           )}
       </section>
