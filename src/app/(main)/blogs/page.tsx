76edc73c14a74ae10eacf832be8a4b54389138a3
muhammedtahir1 <mohammedtahir65080@gmail.com>
2024-10-19 14:36:10 +0530

    implemented blogs using content layer, need some more  improvements

---
+++ src/app/(main)/blogs/page.tsx
@@ -0,0 +1,58 @@
+import { Badge } from '@/components/ui/badge';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { allBlogs , Blog} from 'contentlayer/generated'
+
+function PostCard(blog: Blog) {
+  // const imageLink = `${blog.thumbnail}`;
+
+  return (
+    <Card className="w-[320px]  overflow-hidden">
+      <CardHeader className="p-0 overflow-hidden">
+        {/* {blog.thumbnail && (
+          <Image
+            src={`/images/guides/${blog.thumbnail}`}
+            width={320}
+            height={120}
+            // public\images\guides\Authjs part 1.png
+            alt={`\images\guides\${blog.title}`}
+          />
+        )} */}
+      </CardHeader>
+      <CardContent className="mt-4 pb-4">
+        <CardTitle className="pb-1"> {blog.title}</CardTitle>
+        <CardDescription> {blog.description}</CardDescription>
+      </CardContent>
+      <CardFooter className="py-2 pb-4 flex gap-1 flex-wrap px-5">
+        {blog.tags &&
+          blog.tags.split(",").map((tag, idx) => (
+            <Badge
+              className="font-normal text-xs"
+              variant="default"
+              key={idx}
+            >
+              #{tag.trim()}
+            </Badge>
+          ))}
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default function Page() {
+
+  const blogs = allBlogs
+
+  return (
+    <div className=" py-8">
+      <h1 className="text-4xl font-bold font-passion ">Blogs</h1>
+      <p className="max-w-2xl text-lg  text-muted-foreground mb-8">
+        Blogs to increase you in your knowledge
+      </p>
+      <main className="grid auto-rows-min grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 w-full">
+        {blogs.map((blog, idx) => {
+          if (blog.isPublished) return <PostCard key={idx} {...blog} />;
+        })}
+      </main>
+    </div>
+  )
+}
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -12,14 +12,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="md:flex md:px-8 items-start prose dark:prose-invert">
+    <div className="md:flex  md:px-8 items-start prose dark:prose-invert">
       <aside className="hidden md:sticky md:top-0 md:overflow-hidden md:block md:w-[270px] md:max-w-[270px] md:min-w-[270px] h-screen">
         <ScrollArea className="h-full flex flex-col w-full gap-4 py-6 lg:py-8">
           <Navigation navigation={navigation} />
 
         </ScrollArea>
       </aside>
-      <div className="py-8 px-6 overflow-y-auto">{children}</div>
+      <div className="py-8 px-6 overflow-y-auto flex-1">{children}</div>
       {/* <CallToAction className="mt-10  md:sticky md:top-0 md:overflow-hidden" /> */}
     </div>
   );
